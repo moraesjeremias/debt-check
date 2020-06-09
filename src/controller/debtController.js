44ecb51fbc7bf651ffd47abc4050ad2792636480
moraesjeremias <moraesjeremias@gmail.com>
2020-06-09 15:49:39 -0300

    Adding services and controller functions

--- src/controller/debtController.js
+++ src/controller/debtController.js
@@ -1,8 +1,24 @@
+const fakeDebts = require('../service/getVehicleInfo')
+
 
 module.exports ={
     index(req, res) {
         const placa = req.body.placa
         console.log(placa)
         res.json(placa);
-    }
+    },
+
+    retrieveQueriedDebt(req, res) {
+        const { placa, renavam, uf } = req.body
+        console.log(placa, renavam, uf)
+        try {
+            const queriedDebtResult = fakeDebts.validateVehicleInfo(placa, renavam, uf)
+            return res.json(queriedDebtResult)
+        } catch (error) {
+            console.log(error)
+            return res.json(error)
+        }
+    },
+
+    
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,5 +3,6 @@ const routes = express.Router();
 const debtController = require('../controller/debtController')
 
 routes.get('/test', debtController.index);
+routes.post('/check-debts/', debtController.retrieveQueriedDebt)
 
 module.exports = routes;
